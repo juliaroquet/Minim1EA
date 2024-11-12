7a9f59e1ac5e5ca0539b142bab43f3e1b67da646
julia <[julia.roquet@estudiantat.upc.edu]>
2024-11-12 18:48:59 +0100

    relacion entre coleccion usuario y coleccion mensaje

--- BACKEND/src/models/mensaje.ts
+++ BACKEND/src/models/mensaje.ts
@@ -1,19 +1,41 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import Usuario from './usuario';  // Importar el modelo de Usuario
 
+// Interfaz para el tipo Mensaje
 export interface IMensaje extends Document {
-  remitente: mongoose.Schema.Types.ObjectId;
-  destinatario: mongoose.Schema.Types.ObjectId;
+  remitente: mongoose.Schema.Types.ObjectId; // Relación con el Usuario
+  destinatario: mongoose.Schema.Types.ObjectId; // Relación con el Usuario
   contenido: string;
   etiquetas: string[];
   fecha: Date;
 }
 
+// Definición del esquema de Mensaje
 const MensajeSchema: Schema = new Schema({
-    remitente: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
-    destinatario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
-    contenido: { type: String, required: true },
-    etiquetas: { type: [String], default: [] },
-    fecha: { type: Date, default: Date.now }
-  });
+  remitente: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Usuario', // Establece que el campo 'remitente' es una referencia a la colección 'Usuario'
+    required: true 
+  },
+  destinatario: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Usuario', // Establece que el campo 'destinatario' es una referencia a la colección 'Usuario'
+    required: true 
+  },
+  contenido: { 
+    type: String, 
+    required: true 
+  },
+  etiquetas: { 
+    type: [String], 
+    default: [] 
+  },
+  fecha: { 
+    type: Date, 
+    default: Date.now 
+  }
+}, { versionKey: false });
 
+// Modelo de Mensaje
 export default mongoose.model<IMensaje>('Mensaje', MensajeSchema);
+
